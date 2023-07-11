3d04881d52479ab44a6de605b800d238a50511f4
David Rheinsberg <david@readahead.eu>
2023-07-11 16:18:25 +0200

    build: integrate Rust library into code-base
    Add a rust library to the dbus-broker code-base. Pull in the
    `meson-cargo` subproject to integrate `Cargo` based Rust code into the
    Meson build system.
    
    The integration syntax is a bit awkward, given that Meson does not allow
    custom modules or custom functions. Hence, we have to use dict-expansion
    to integrate `meson-cargo` in meson.build. While awkward to read, it
    works fine.
    
    So far, only a dummy libdbus_broker.a is built, which carries no
    modules. Later patches will add actual Rust code.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! D-Bus Broker Support Library
+//!
+//! This rust library provides all the implementation details of the entire
+//! dbus-broker code-base. It is compiled into a single archive and then linked
+//! into each target, if needed. The linker is expected to strip all unused
+//! parts of the library.
